af531bd6885e5fde2ccfc2ede2b7227197d29342
alexandriai168 <alexandria.kay.im@gmail.com>
2023-03-20 14:43:29 -0700

    description added to metabarcoding wrapper

--- metabarcoding_wrapper.sh
+++ metabarcoding_wrapper.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# Metabarcoding Wrapper
+# written by: Alexandria Im
+# This wrapper takes in _______ as inputs and has an interactive interface that allows you to run the entire pipeline and decide what primers/databases to use for the analysis.
+# USAGE: bash metabarcoding_wrapper.sh {INPUT_FASTQ}
+
 # NOTE : NEED TO CHANGE PATHWAYS TO PATHWAYS ON SEDNA. CURRENTLY SET TO DIRECTORY STRUCTURE ON CEG
 INPUT_FASTQ = ${1}
 ALL_TAX_REF = ~/aim/muri_project/mod3/example_dir_structure/metadata/NCBI_mtDNA_alltaxa_reference.fasta
 ALL_PRIMER_DATA = ~/aim/muri_project/mod3/example_dir_structure/metadata/MURI301.csv
 TAX_REF_2 = ${4}
 
-# STEP 0: cd into pipeline directory and move input
+#################### STEP 0: cd into pipeline directory and move input ####################
 cd ~/aim/muri_project/mod3/example_dir_structure
 mv ./fastq_holding_pen/${INPUT_FASTQ} ./raw_fastqs/
+###############################################################
 
 #################### STEP 1: DADA2 ####################
 
@@ -78,12 +84,22 @@ mv
 
 ###############################################################
 
-# STEP 3: Generate Report
+#################### STEP 3: Generate Report ####################
+
+Rscript -e "rmarkdown::render('example.Rmd',params=list(args = myarg))"
+
+###############################################################
 
 
+# TO-DO:
 # look into if you can do rmd in this context?
 # do we want it to ask all questions at the beginning of the run? or is it ok to ask when step 2 starts for example?
+# asv/hashing for dada2 step
+# enabling inputs for the R scripts
 
+# look into why we wanna 
+# for trimming, might be trimming way too much for the longer ones like Dloop (429 trimmed to 230)
+# look into calculating where to trim 
 
 # DONE:
 # gotta work on WHERE the outputs go and the general directory structure
