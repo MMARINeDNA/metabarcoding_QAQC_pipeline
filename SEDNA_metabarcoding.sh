d4f397451b4b9a4ca3693815aa77c5a9b85147da
alexandriai168 <alexandria.kay.im@gmail.com>
2023-03-20 16:37:27 -0700

    adding HPC script and updated primers for wrapper

--- SEDNA_metabarcoding.sh
+++ SEDNA_metabarcoding.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+#SBATCH --partition=node # Queue selection
+#SBATCH --job-name=build_reference_database_sedna# Job name
+#SBATCH --mail-type=ALL # Mail events (BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user= [EMAIL HERE] # Where to send mail\
+#SBATCH --ntasks=1 # Run a single task\
+#SBATCH --cpus-per-task=20  # Number of CPU cores per task\
+#SBATCH --mem=10000 # Job memory request\
+#SBATCH --time=24:00:00 # Time limit hrs:min:sec\
+#SBATCH --output=build_reference_database.log # Standard output/error\
+#export OMP_NUM_THREADS=8
+
+
 # Metabarcoding Wrapper
 # written by: Alexandria Im
 # This wrapper takes in _______ as inputs and has an interactive interface that allows you to run the entire pipeline and decide what primers/databases to use for the analysis.
 # USAGE: bash metabarcoding_wrapper.sh {INPUT_FASTQ}
 
+module load R
+
 # NOTE : NEED TO CHANGE PATHWAYS TO PATHWAYS ON SEDNA. CURRENTLY SET TO DIRECTORY STRUCTURE ON CEG
 INPUT_FASTQ = ${1}
 ALL_TAX_REF = ${2}
 ALL_PRIMER_DATA = ~/aim/muri_project/mod3/example_dir_structure/metadata/MURI301.csv
-TAX_REF_2 = ${4}
+
 
 #################### STEP 0: cd into pipeline directory and move input ####################
 cd ~/aim/muri_project/mod3/example_dir_structure
@@ -79,27 +93,3 @@ mv
 Rscript -e "rmarkdown::render('Primer_test_prelim_report.qmd',params=list(args = myarg))"
 
 ###############################################################
-
-
-# TO-DO:
-# change output for assignTax directory IN R CODE
-# do we want it to ask all questions at the beginning of the run? or is it ok to ask when step 2 starts for example?
-# asv/hashing for dada2 step
-# change for loop in dada2 script to not break when it hits a primer that isn't there
-# enabling inputs for the R scripts
-# holding pen will be on QNAP and rest will be on SEDNA... need to figure out how to automatically move
-# note that DADA2
-
-# DONE:
-# gotta work on WHERE the outputs go and the general directory structure
-# ask again and again to do step 2 until you're done
-# ask if we want to do the assign taxonomy step again
-# ask for multiple inputs
-
-
-
-
-
-
-
-
