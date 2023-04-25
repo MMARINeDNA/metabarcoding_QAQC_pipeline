16586f8020139cce41344dd37833da49537e7a3b
alexandriai168 <alexandria.kay.im@gmail.com>
2023-04-25 16:41:43 -0700

    fix bug in primer cutadapt script- was empty

---
+++ primer_trimming.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#################### PRIMER VARIABLE ASSIGNMENT ####################
+MFU_F="GCCGGTAAAACTCGTGCCAGC"
+MFU_R="CATAGTGGGGTATCTAATCCCAGTTTG"
+DL_F="TCACCCAAAGCTGRARTTCTA"
+DL_R="GCGGGTTGCTGGTTTCACG"
+MV1_F="CGTGCCAGCCACCGCG"
+MV1_R="GGGTATCTAATCCYAGTTTG"
+C16_F="GACGAGAAGACCCTAWTGAGCT"
+C16_R="AAATTACGCTGTTATCCCT"
+
+
+CUTADAPT=$(which cutadapt)
+for i in *R1*
+do
+FILE_PRIM=$(echo ${i} | cut -d - -f 1) #grab primer name at beginning of file name
+FILE_NAME=$(echo ${i} | cut -d _ -f 1,2,3) #grab everything before R1 (aka sample name)
+R1=${i}
+R2=$(echo ${i} | sed 's/R1/R2/g')
+if [[ ${FILE_PRIM} == "MFU" ]]; then
+echo MFU Detected
+${CUTADAPT} -g ${MFU_F} \
+     -G "${MFU_R}" \
+     -o ../for_dada2/${R1} \
+     -p ../for_dada2/${R2} \
+    --discard-untrimmed \
+    -j 0 \
+"${R1}" "${R2}" 1> "../cutadapt_reports/${FILE_NAME}_trim_report.txt"
+elif [[ ${FILE_PRIM} == "DL" ]]; then
+echo DL Detected
+${CUTADAPT} -g ${DL_F} \
+     -G "${DL_R}" \
+     -o ../for_dada2/${R1} \
+     -p ../for_dada2/${R2} \
+    --discard-untrimmed \
+    -j 0 \
+"${R1}" "${R2}" 1> "../cutadapt_reports/${FILE_NAME}_trim_report.txt"
+elif [[ ${FILE_PRIM} == "MV1" ]]; then
+echo MV1 Detected
+${CUTADAPT} -g ${MV1_F} \
+     -G "${MV1_R}" \
+     -o ../for_dada2/${R1} \
+     -p ../for_dada2/${R2} \
+    --discard-untrimmed \
+    -j 0 \
+"${R1}" "${R2}" 1> "../cutadapt_reports/${FILE_NAME}_trim_report.txt"
+elif [[ ${FILE_PRIM} == "C16" ]]; then
+echo MV1 Detected
+${CUTADAPT} -g ${C16_F} \
+     -G "${C16_R}" \
+     -o ../for_dada2/${R1} \
+     -p ../for_dada2/${R2} \
+    --discard-untrimmed \
+    -j 0 \
+"${R1}" "${R2}" 1> "../cutadapt_reports/${FILE_NAME}_trim_report.txt"
+
+fi
+done
