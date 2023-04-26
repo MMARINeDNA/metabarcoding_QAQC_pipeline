1ff98182154053db93b4b008fbc0cc5ff47e42ef
Ryan Kelly <rpkelly@uw.edu>
2023-04-26 14:08:57 -0700

    fixed mkdir typo

--- config.sh
+++ config.sh
@@ -5,7 +5,7 @@ PATH_TO_DIR=${1}
 echo Configuring file system ... $(date +"%T")
 mkdir ${PATH_TO_DIR}/muri_metabarcoding
 mkdir ${PATH_TO_DIR}/muri_metabarcoding/raw_fastqs
-mkidr ${PATH_TO_DIR}/muri_metabarcoding/for_dada2
+mkdir ${PATH_TO_DIR}/muri_metabarcoding/for_dada2
 mkdir ${PATH_TO_DIR}/muri_metabarcoding/final_data
 mkdir ${PATH_TO_DIR}/muri_metabarcoding/final_data/csv_output
 mkdir ${PATH_TO_DIR}/muri_metabarcoding/final_data/rdata_output
