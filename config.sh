5dbb8ed2c3ffea853e13410ff3d212d9126b2cb9
alexandriai168 <alexandria.kay.im@gmail.com>
2023-05-01 16:25:03 -0700

    changed config to create in git repo

--- config.sh
+++ config.sh
@@ -3,21 +3,17 @@
 PATH_TO_DIR=${1}
 
 echo Configuring file system ... $(date +"%T")
-mkdir ${PATH_TO_DIR}/muri_metabarcoding
-mkdir ${PATH_TO_DIR}/muri_metabarcoding/raw_fastqs
-mkdir ${PATH_TO_DIR}/muri_metabarcoding/for_dada2
-mkdir ${PATH_TO_DIR}/muri_metabarcoding/final_data
-mkdir ${PATH_TO_DIR}/muri_metabarcoding/final_data/csv_output
-mkdir ${PATH_TO_DIR}/muri_metabarcoding/final_data/rdata_output
-mkdir ${PATH_TO_DIR}/muri_metabarcoding/final_data/logs
-mkdir ${PATH_TO_DIR}/muri_metabarcoding/analysis_output
-mkdir ${PATH_TO_DIR}/muri_metabarcoding/scripts
-mkdir ${PATH_TO_DIR}/muri_metabarcoding/metadata
-mkdir ${PATH_TO_DIR}/muri_metabarcoding/metadata/known_hashes
-mkdir ${PATH_TO_DIR}/muri_metabarcoding/cutadapt_reports
+mkdir ${PATH_TO_DIR}/
+mkdir ${PATH_TO_DIR}/raw_fastqs
+mkdir ${PATH_TO_DIR}/for_dada2
+mkdir ${PATH_TO_DIR}/final_data
+mkdir ${PATH_TO_DIR}/final_data/csv_output
+mkdir ${PATH_TO_DIR}/final_data/rdata_output
+mkdir ${PATH_TO_DIR}/final_data/logs
+mkdir ${PATH_TO_DIR}/analysis_output
+mkdir ${PATH_TO_DIR}/scripts
+mkdir ${PATH_TO_DIR}/metadata
+mkdir ${PATH_TO_DIR}/metadata/known_hashes
+mkdir ${PATH_TO_DIR}/cutadapt_reports
 echo Finished creating file system ... $(date +"%T")
-
-echo Copying over necessary files ... $(date +"%T")
-cp  -r ./data/* ${PATH_TO_DIR}/muri_metabarcoding/metadata
-cp   ./bin/* ${PATH_TO_DIR}/muri_metabarcoding/scripts
 echo Config finished! $(date +"%T")
