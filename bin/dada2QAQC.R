550b0e48f75b671419b9cd0e3b48e2e02a3e5286
taniavaldiviac <tania.valdiviac@gmail.com>
2023-05-17 12:35:53 -0700

    Update dada2QAQC.R
    matchID=TRUE added in line96 to avoid error

--- bin/dada2QAQC.R
+++ bin/dada2QAQC.R
@@ -93,7 +93,7 @@ for (i in 1:nrow(primer.data)){
     print(paste0("Starting filter and trim...", Sys.time()))
     out <- filterAndTrim(fnFs, filtFs, fnRs, filtRs, 
                          trimRight = c(primer.data$primer_length_r[i],primer.data$primer_length_f[i]),
-                         truncLen = c(where_trim_all_Fs,where_trim_all_Rs),
+                         truncLen = c(where_trim_all_Fs,where_trim_all_Rs),matchIDs=TRUE,
                           maxN=0, maxEE=c(2,2), truncQ=2, rm.phix=TRUE,
                            compress=TRUE, multithread=TRUE)
     print(paste0("Finished filter and trim.  ", Sys.time()))
