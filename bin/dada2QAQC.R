97fafda8c1c4d76cdb30d1fbb49ffb125c623436
alexandriai168 <alexandria.kay.im@gmail.com>
2023-05-31 12:26:55 -0700

    added save filtered out asvs

--- bin/dada2QAQC.R
+++ bin/dada2QAQC.R
@@ -12,7 +12,7 @@ library(digest)
 
 
 ### read in input files and variables ------------------------------------------------------
-#args <- commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly=TRUE)
 fastq_location <- args[1]
 output_location <- args[2]
 metadata_location <- args[3]
@@ -149,6 +149,8 @@ for (i in 1:nrow(primer.data)){
 ### Filter by Size ---------------------------------------------------------------
     indexes.to.keep <- which(nchar(colnames(seqtab.nochim)) < primer.data$amplicon_length[i])
     cleaned.seqtab.nochim <- seqtab.nochim[,indexes.to.keep]
+    filteredout.seqtab.nochim <- seqtab.nochim[,!indexes.to.keep]
+    write.csv(filteredout.seqtab.nochim,paste0(output_location,"logs","filtered_out_asv.csv"))
     
 ### Track reads through pipeline ---------------------------------------------------------------
     getN <- function(x) sum(getUniques(x))
