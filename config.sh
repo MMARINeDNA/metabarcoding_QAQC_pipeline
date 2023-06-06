30003e478d2d6f402b94e1c491ce635175dcf832
alexandriai168 <alexandria.kay.im@gmail.com>
2023-06-06 16:06:19 -0700

    makes muri_metabarcoding directory

--- config.sh
+++ config.sh
@@ -3,17 +3,17 @@
 PATH_TO_DIR=${1}
 
 echo Configuring file system ... $(date +"%T")
-mkdir ${PATH_TO_DIR}/raw_fastqs
-mkdir ${PATH_TO_DIR}/for_dada2
-mkdir ${PATH_TO_DIR}/final_data
-mkdir ${PATH_TO_DIR}/final_data/csv_output
-mkdir ${PATH_TO_DIR}/final_data/rdata_output
-mkdir ${PATH_TO_DIR}/final_data/logs
-mkdir ${PATH_TO_DIR}/analysis_output
-mkdir ${PATH_TO_DIR}/cutadapt_reports
+mkdir ${PATH_TO_DIR}/muri_metabarcoding/raw_fastqs
+mkdir ${PATH_TO_DIR}/muri_metabarcoding/for_dada2
+mkdir ${PATH_TO_DIR}/muri_metabarcoding/final_data
+mkdir ${PATH_TO_DIR}/muri_metabarcoding/final_data/csv_output
+mkdir ${PATH_TO_DIR}/muri_metabarcoding/final_data/rdata_output
+mkdir ${PATH_TO_DIR}/muri_metabarcoding/final_data/logs
+mkdir ${PATH_TO_DIR}/muri_metabarcoding/analysis_output
+mkdir ${PATH_TO_DIR}/muri_metabarcoding/cutadapt_reports
 
 
 echo Copying over necessary files ... $(date +"%T")
-cp  -r ./metadata/* ${PATH_TO_DIR}/metadata
-cp   ./bin/* ${PATH_TO_DIR}/scripts
+cp  -r ./metadata/* ${PATH_TO_DIR}/muri_metabarcoding/metadata
+cp   ./bin/* ${PATH_TO_DIR}/muri_metabarcoding/scripts
 echo Config finished! $(date +"%T")
