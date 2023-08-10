aeeb5f1050f4461b153fbc07905e6894103c89b9
alexandriai168 <alexandria.kay.im@gmail.com>
2023-08-10 16:38:37 -0700

    added dependencies installer

---
+++ install_depenencies.R
@@ -0,0 +1,3 @@
+### Installing R dependencies
+install.packages(c("tidyverse","seqinr","digest","quarto","rmarkdown","pander","pandoc","ggplot2","dplyr","strex","sqldf","knitr","rmdformats","data.table","filesstrings","BiocManager"))
+BiocManager::install(c("ShortRead","phyloseq","dada2"))
